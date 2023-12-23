e7ad1e9d354218cbde390b00bb706a32050f5de0
DaniloHenriqueLira <96589393+DaniloHenriqueLira@users.noreply.github.com>
2023-12-23 21:19:08 +0000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-// Função para obter parâmetros da URL
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -9,7 +9,7 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-// Função para atualizar os botões com base nos valores da URL
+
 function updateButtonsFromURL() {
     var buttons = ['start', 'reset', 'reversao', 'jog'];
     buttons.forEach(function (button) {
@@ -21,31 +21,29 @@ function updateButtonsFromURL() {
     });
 }
 
-// Função para alternar o valor e atualizar o estilo
+
 function toggleValue(inputName) {
     var inputValue = document.querySelector(`[name='"date".${inputName}']`);
     inputValue.value = inputValue.value === '1' ? '0' : '1';
     updateButtonStyle(inputName, inputValue.value);
     updateURL(inputName, inputValue.value);
-    // Recarregar a página
-    location.reload();
-    
+   
 }
 
-// Função para atualizar o estilo do botão com base no valor
+
 function updateButtonStyle(inputName, inputValue) {
     var inputButton = document.querySelector(`.${inputName}Button`);
     inputButton.classList.toggle('active', inputValue === '1');
 }
 
-// Função para atualizar a URL
+
 function updateURL(inputName, inputValue) {
     var baseUrl = window.location.href.split('?')[0];
     var newUrl = baseUrl + `?"date".${inputName}=${inputValue}`;
     window.history.replaceState({}, document.title, newUrl);
 }
 
-// Associar a função a cada botão
+
 document.querySelector('.startButton').addEventListener('click', function () {
     toggleValue('start');
 });
@@ -62,6 +60,6 @@ document.querySelector('.jogButton').addEventListener('click', function () {
     toggleValue('jog');
 });
 
-// Atualizar botões ao carregar a página
+
 updateButtonsFromURL();
 
